527c75b11bc58e8c33dee13275b9275eefbeb089
Bernabé Gonzalez García <bernabegoga@gmail.com>
2018-11-21 12:13:53 +0100

    Removed MyAsyncTask

--- app/src/main/java/upf/internetofthings/SyncActivity.java
+++ app/src/main/java/upf/internetofthings/SyncActivity.java
@@ -1,13 +1,22 @@
 package upf.internetofthings;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import upf.internetofthings.utilities.MyAsyncTask;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 public class SyncActivity extends AppCompatActivity implements View.OnClickListener {
     private final static int INTERVAL = 2000; //2 minutes
@@ -47,6 +56,43 @@ public class SyncActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+
+    }
+
+    public class MyAsyncTask extends AsyncTask<String, Object, Object> {
+
+        private Object resp;
+        ProgressDialog progressDialog;
+
+
+        @Override
+        protected Object doInBackground(String... params) {
+            resp = requestHttp();
+            return resp;
+        }
+
+        private Object requestHttp() {
+            publishProgress("Connecting..."); // Calls onProgressUpdate()
+            try {
+                URL url = new URL("https://www.ara.cat/rss/esports/");
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.setRequestMethod("GET");
+                DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+                DocumentBuilder db = dbf.newDocumentBuilder();
+                Document doc = db.parse(url.openStream());
+
+
+                NodeList titles = doc.getElementsByTagName("title");
+                for (int i = 0; i < titles.getLength(); i++) {
+                    Log.i("INFOOOOOOOOOOOOOOOOOOOO", titles.item(i).getTextContent());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                resp = e.getMessage();
+            }
+            return resp;
+        }
+
 
     }
 }
